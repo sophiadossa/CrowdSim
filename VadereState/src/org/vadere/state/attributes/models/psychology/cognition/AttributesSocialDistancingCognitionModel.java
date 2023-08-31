dae7fcd5e12f2d12c19bf29d196f59246a5fc308
Ludwig Jäck <ludwig.jaeck@outlook.de>
2023-08-31 09:36:56 +0200

    updated comments in AttributesSocialDistancingCognitionModel

--- VadereState/src/org/vadere/state/attributes/models/psychology/cognition/AttributesSocialDistancingCognitionModel.java
+++ VadereState/src/org/vadere/state/attributes/models/psychology/cognition/AttributesSocialDistancingCognitionModel.java
@@ -2,24 +2,25 @@ package org.vadere.state.attributes.models.psychology.cognition;
 
 /**
  * This model decides whether agents perform social distancing or not.
- * It perfoms social distancing by modifying the agents repulsion.
- * (See https://doi.org/10.17815/CD.2021.116)
+ * It performs social distancing by modifying the potential strategy.
+ * (See <a href="https://doi.org/10.17815/CD.2021.116">)
  */
 public class AttributesSocialDistancingCognitionModel extends AttributesCognitionModel {
     /**
-     *
+     * <repulsionFactor> factor for social distancing
      */
     private double repulsionFactor = 1.6444;
     /**
-     *
+     * <i> repulsionIntercept</i> sum of the parameters found with regression 0.0658c + 0.6161
+     * where the corridor width c is fixed c = 2
      */
     private double repulsionIntercept  = 0.4845;
     /**
-     *
+     * <minDistance> minimum distance for social distancing
      */
     private double minDistance = 1.25;
     /**
-     *
+     * <maxDistance> maximum distance for social distancing
      */
     private double maxDistance = 2.0;
 
